edc124e0bbbef2d1e43d6bf8448c81dbceadbb09
Oleksandr Kostyuk <75908349+alex-kostuyk@users.noreply.github.com>
2023-09-26 19:58:19 +0300

    add auto port search

--- inputOutputSubmarine.py
+++ inputOutputSubmarine.py
@@ -1,17 +1,36 @@
 import serial
+import serial.tools.list_ports
 import struct
 import time
 import numpy
 
-ser = serial.Serial('COM9', 9600)
-serJoy = serial.Serial('COM5', 9600)
+def findSerialPortByName(Name):
+    available_ports = list(serial.tools.list_ports.comports())
+    for port in available_ports:
+        if Name in port.description:
+            return port.device
+    return None
+
+        
+        
+ser = serial.Serial(findSerialPortByName('USB-SERIAL CH340'), 9600)
+
+joystick_device = findSerialPortByName('Arduino Mega 2560')
+
+if joystick_device!=None:
+    serJoy = serial.Serial(joystick_device, 9600)
+
+
 
 update_interval = 0.7
 last_update_time = time.time()
 
 
 while True:
-    data = numpy.array(serJoy.readline().decode('utf-8').strip().split(' ')).astype(int)
+    if joystick_device!=None:
+        data = numpy.array(serJoy.readline().decode('utf-8').strip().split(' ')).astype(int)
+    #else:
+        
 
     data = numpy.append(data, 0)
 
@@ -23,12 +42,10 @@ while True:
     for value in data:
         packed_data += struct.pack('B', value)
 
-    # Send the packed data
     ser.write(packed_data)
 
     time.sleep(0.02)
 
-    # Receive and print telemetry
     if ser.inWaiting() > 0:
         telemetry = struct.unpack("BBBB", ser.read(4))
         temp = telemetry[0]/5
